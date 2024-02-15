7742b1bec2722daa76d8b094527721b253368451
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-02-15 05:48:06 -0300

    ci: better test versions

--- test/compatibility/bun-0.5.3.test.ts
+++ test/compatibility/bun-0.5.3.test.ts
@@ -2,5 +2,5 @@ import process from 'node:process';
 import { executeDockerCompose } from '../helpers/check-node.test.js';
 
 (async () => {
-  process.exit(await executeDockerCompose('bun-0-5-3'));
+  process.exit(await executeDockerCompose('bun-0.5.3'));
 })();
--- test/compatibility/deno-1.30.0.test.ts
+++ test/compatibility/deno-1.30.0.test.ts
@@ -2,5 +2,5 @@ import process from 'node:process';
 import { executeDockerCompose } from '../helpers/check-node.test.js';
 
 (async () => {
-  process.exit(await executeDockerCompose('deno-1-30-0'));
+  process.exit(await executeDockerCompose('deno-1.30.0'));
 })();
