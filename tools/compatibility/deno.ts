75ce41e8baed58a9f8567771ff201b982d4b6321
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-03-03 17:43:03 -0300

    chore: fix process never ends

--- tools/compatibility/deno.ts
+++ tools/compatibility/deno.ts
@@ -1,19 +1,15 @@
 // eslint-disable-next-line import/no-unresolved
-import { listFiles } from 'https://esm.sh/poku';
+import { listFiles } from 'npm:poku';
 
-const ensureDenoCompatibility = async (path: string) => {
-  const files = listFiles(path, {
-    filter: /\.ts/,
-  });
+const files = listFiles('.', {
+  filter: /\.ts/,
+});
 
-  console.log('Ensuring Deno Compatibility For:', files);
+console.log('Ensuring Deno Compatibility For:', files);
 
-  for await (const file of files) {
-    const raw = await Deno.readTextFile(file);
-    const content = raw.replace(/((import|export|from).+)(\.js)/g, '$1.ts');
+for (const file of files) {
+  const raw = await Deno.readTextFile(file);
+  const content = raw.replace(/((import|export|from).+)(\.js)/g, '$1.ts');
 
-    await Deno.writeTextFile(file, content);
-  }
-};
-
-await ensureDenoCompatibility('.');
+  await Deno.writeTextFile(file, content);
+}
