b2d2addb1a258d9868d7bec1cd8322816536ca7c
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-03-03 11:43:35 -0300

    fix: allows local variables in beforeEach

---
+++ src/helpers/find-file.ts
@@ -0,0 +1,31 @@
+import { EOL } from 'node:os';
+
+export const findFile = (error: Error) => {
+  const stackLines = error.stack?.split(EOL) || [];
+
+  let file = '';
+
+  const basePath = 'poku/lib/';
+
+  for (const line of stackLines) {
+    if (!line.includes(basePath)) {
+      const match = line.match(
+        /at\s(\/.+|file:.+)|^(\s+)at\smodule\scode\s\((\/.+|file:.+)\)/i
+      );
+
+      // Node and Deno
+      if (match && match[1]) {
+        file = match[1];
+        break;
+      }
+
+      // Bun
+      if (match && match[3]) {
+        file = match[3];
+        break;
+      }
+    }
+  }
+
+  return file;
+};
