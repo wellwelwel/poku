08d4734fd7bad8dd61f29d8bdab80c735e0b230f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-03-14 19:28:32 -0300

    docs: fix inline docs

--- src/@types/poku.ts
+++ src/@types/poku.ts
@@ -57,11 +57,11 @@ export type Configs = {
    * Ex.:
    *
    * ```ts
-   * beforeEach(prepare)
+   * beforeEach(() => myFunc())
    * ```
    *
    * ```ts
-   * beforeEach('/tools/prepare.ts')
+   * beforeEach(async () => await myFunc())
    * ```
    */
   beforeEach?: () => unknown | Promise<unknown>;
@@ -71,11 +71,11 @@ export type Configs = {
    * Ex.:
    *
    * ```ts
-   * afterEach(cleanup)
+   * afterEach(() => myFunc())
    * ```
    *
    * ```ts
-   * afterEach('/tools/cleanup.ts')
+   * afterEach(async () => await myFunc())
    * ```
    */
   afterEach?: () => unknown | Promise<unknown>;
