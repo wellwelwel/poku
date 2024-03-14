7c59b764ad5dff9e047204645b1092f20588a531
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-03-14 19:47:41 -0300

    chore(tools): specify paths manually

--- tools/compatibility/deno.ts
+++ tools/compatibility/deno.ts
@@ -1,17 +1,22 @@
 // eslint-disable-next-line import/no-unresolved
 import { listFiles } from 'npm:poku';
 
-const files = listFiles('.', {
-  filter: /\.ts/,
-});
+const ensureNodeCompatibility = async (path: string) => {
+  const files = listFiles(path, {
+    filter: /\.ts/,
+  });
 
-console.log('Ensuring Deno Compatibility For:', files);
+  console.log('Ensuring Deno Compatibility For:', files);
 
-for await (const file of files) {
-  const raw = await Deno.readTextFile(file);
-  const content = raw.replace(/((import|export|from).+)(\.js)/g, '$1.ts');
+  for await (const file of files) {
+    const raw = await Deno.readTextFile(file);
+    const content = raw.replace(/((import|export|from).+)(\.js)/g, '$1.ts');
 
-  await Deno.writeTextFile(file, content);
-}
+    await Deno.writeTextFile(file, content);
+  }
+};
+
+await ensureNodeCompatibility('./src');
+await ensureNodeCompatibility('./test');
 
 Deno.exit(0);
