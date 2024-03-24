089522dc260af0f377b0f7fe5b24578f7c9db901
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-03-24 00:13:59 -0300

    ci: remove Deno 1.30.0 from matrix due to several inconsistencies

--- test/compatibility/deno-1.30.0.test.ts
+++
@@ -1,6 +0,0 @@
-import process from 'node:process';
-import { executeDockerCompose } from '../helpers/check-node.test.js';
-
-(async () => {
-  process.exit(await executeDockerCompose('deno-1.30.0'));
-})();
