cee922ef1d7beeceded60294eb5863bd686111bc
Roberto <robertolemesp@hotmail.com>
2024-04-29 18:07:38 -0300

    Increments coverage adding each service unit tests

---
+++ test/unit/each.test.ts
@@ -0,0 +1,113 @@
+import {
+  describe,
+  assert,
+  beforeEach,
+  afterEach,
+  test,
+} from '../../src/index.js';
+
+describe('Before and After Each Suite', {
+  background: false,
+  icon: '🔬',
+});
+
+let counter = 0;
+
+const beforeEachHelper = beforeEach(
+  () => {
+    ++counter;
+  },
+  { immediate: true }
+);
+
+const afterEachHelper = afterEach(() => {
+  ++counter;
+}, {});
+
+test(() => {
+  assert.equal(
+    counter,
+    2,
+    'value incremented by 1 from beforeEach with immediate option should be 2'
+  );
+});
+
+test(() => {
+  assert.equal(
+    counter,
+    4,
+    'value incremented by 1 from beforeEach with immediate option and by 1 from previous test with afterEach should be 4'
+  );
+});
+
+afterEachHelper.pause();
+beforeEachHelper.pause();
+
+test(() => {
+  assert.equal(
+    counter,
+    5,
+    'value should still 5 by pausing both beforeEach and afterEach, considering the abscence of beforeEach effect'
+  );
+});
+
+test(() => {
+  assert.equal(
+    counter,
+    5,
+    'value should still 5 by pausing both beforeEach and afterEach, considering the the abscence of beforeEach and afterEach (from previous test) effects'
+  );
+});
+
+afterEachHelper.continue();
+beforeEachHelper.continue();
+
+test(() => {
+  assert.equal(
+    counter,
+    6,
+    'value incremented by 1 from unpausing beforeEach should be 6'
+  );
+});
+
+test(() => {
+  assert.equal(
+    counter,
+    8,
+    'value incremented by 1 from beforeEach and 1 by previous test with afterEach should be 8'
+  );
+});
+
+beforeEachHelper.reset();
+afterEachHelper.reset();
+
+test(() => {
+  assert.equal(
+    counter,
+    9,
+    'value should still 9 by reseting both beforeEach and afterEach'
+  );
+});
+
+// test(() => {
+//   let counter = 0;
+
+//   const asyncPreIncrement = async () =>
+//     await new Promise((resolve) => resolve(++counter));
+
+//   beforeEach(asyncPreIncrement, { immediate: true });
+
+//   afterEach(asyncPreIncrement, {});
+
+//   assert.equal(
+//     counter,
+//     1,
+//     'value asynchronously incremented by 1 from beforeEach should be 1'
+//   );
+
+//   assert.equal(
+//     counter,
+//     3,
+//     'same value asynchronously incremented again by 1 from beforeEach and also by afterEach should be 3 now and then will be 4'
+//   );
+// });
