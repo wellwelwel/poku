4db3d9fe0bf47db106c4143b24541bd5c30a1428
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-05-15 18:18:18 -0300

    chore: remove silient throw

--- src/services/pid.ts
+++ src/services/pid.ts
@@ -6,93 +6,77 @@ import { EOL } from 'node:os';
 export const killPID = {
   unix: (PID: number): Promise<void> =>
     new Promise((resolve) => {
-      try {
-        const service = spawn('kill', ['-9', String(PID)]);
+      const service = spawn('kill', ['-9', String(PID)]);
 
-        service.on('close', () => {
-          resolve(undefined);
-        });
-      } catch {
+      service.on('close', () => {
         resolve(undefined);
-      }
+      });
     }),
   windows: (PID: number): Promise<void> =>
     new Promise((resolve) => {
-      try {
-        const service = spawn('taskkill', ['/F', '/T', '/PID', String(PID)]);
+      const service = spawn('taskkill', ['/F', '/T', '/PID', String(PID)]);
 
-        service.on('close', () => {
-          resolve(undefined);
-        });
-      } catch {
+      service.on('close', () => {
         resolve(undefined);
-      }
+      });
     }),
 };
 
 export const getPIDs = {
   unix: (port: number): Promise<number[]> =>
     new Promise((resolve) => {
-      try {
-        const PIDs: Set<number> = new Set();
-        const service = spawn('lsof', [
-          '-t',
-          '-i',
-          `:${Number(port)}`,
-          '-s',
-          'TCP:LISTEN',
-        ]);
+      const PIDs: Set<number> = new Set();
+      const service = spawn('lsof', [
+        '-t',
+        '-i',
+        `:${Number(port)}`,
+        '-s',
+        'TCP:LISTEN',
+      ]);
 
-        service.stdout.on('data', (data: Buffer) => {
-          const output = data.toString().trim().split(EOL);
+      service.stdout.on('data', (data: Buffer) => {
+        const output = data.toString().trim().split(EOL);
 
-          output.forEach((pid) => {
-            if (pid) PIDs.add(Number(pid));
-          });
+        output.forEach((pid) => {
+          if (pid) PIDs.add(Number(pid));
         });
+      });
 
-        service.on('close', () => {
-          resolve(Array.from(PIDs));
-        });
-      } catch {
-        resolve([]);
-      }
+      service.on('close', () => {
+        resolve(Array.from(PIDs));
+      });
     }),
   windows: (port: number): Promise<number[]> =>
     new Promise((resolve) => {
-      try {
-        const PIDs: Set<number> = new Set();
-        const service = spawn('cmd.exe', [
-          '/c',
-          `netstat -aon | findstr :${Number(port)}`,
-        ]);
+      const PIDs: Set<number> = new Set();
+      const service = spawn('cmd.exe', [
+        '/c',
+        `netstat -aon | findstr :${Number(port)}`,
+      ]);
 
-        service.stdout.on('data', (data: Buffer) => {
-          const output = data.toString().trim();
-          const lines = output.trim().split(EOL);
+      service.stdout.on('data', (data: Buffer) => {
+        const output = data.toString().trim();
+        const lines = output.trim().split(EOL);
 
-          /**
-           * TODO: Chack line for "/:\d+\s+\w+\s+\d+\s+(\d+)/" regex match to safe support multiple Windows versions
-           * (Tested against ReDos Checker)
-           */
-          lines.map((line) => {
-            const tokens = line.trim().split(/\s+/);
-            const stateIndex = tokens.indexOf('LISTENING');
+        /**
+         * TODO: Chack line for "/:\d+\s+\w+\s+\d+\s+(\d+)/" regex match to safe support multiple Windows versions
+         * (Tested against ReDos Checker)
+         */
+        lines.map((line) => {
+          const tokens = line.trim().split(/\s+/);
+          const stateIndex = tokens.indexOf('LISTENING');
 
-            if (stateIndex !== -1 && tokens[stateIndex + 1]) {
-              const pid = Number(tokens[stateIndex + 1]);
+          if (stateIndex !== -1 && tokens[stateIndex + 1]) {
+            const pid = Number(tokens[stateIndex + 1]);
 
-              if (!isNaN(pid)) PIDs.add(pid);
-            }
-          });
+            if (!isNaN(pid)) PIDs.add(pid);
+          }
         });
+      });
 
-        service.on('close', () => {
-          resolve(Array.from(PIDs));
-        });
-      } catch {
-        resolve([]);
-      }
+      service.on('close', () => {
+        resolve(Array.from(PIDs));
+      });
     }),
 };
 
