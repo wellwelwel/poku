87421217e56e28302f131e5b44b997f1e8e383a5
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-05-15 19:03:25 -0300

    ci: add test when no port is passed to end method

--- test/e2e/node/background-process.test.ts
+++ test/e2e/node/background-process.test.ts
@@ -13,7 +13,7 @@ import { getRuntime } from '../../../src/helpers/get-runtime.js';
   const runtime = getRuntime();
 
   await test(async () => {
-    describe('Start Service', { background: false, icon: '🔀' });
+    describe('Start Service (Single Port)', { background: false, icon: '🔀' });
 
     const server = await startService(`server-a.${ext}`, {
       startAfter: 'ready',
@@ -34,7 +34,7 @@ import { getRuntime } from '../../../src/helpers/get-runtime.js';
   });
 
   await test(async () => {
-    describe('Start Script', { background: false, icon: '🔀' });
+    describe('Start Script (Single Port)', { background: false, icon: '🔀' });
 
     const server = await startScript(`start:${ext}`, {
       startAfter: 'ready',
@@ -103,4 +103,59 @@ import { getRuntime } from '../../../src/helpers/get-runtime.js';
 
     await server.end([4001]);
   });
+
+  if (runtime === 'node') {
+    await test(async () => {
+      describe('Start Service (No Ports)', {
+        background: false,
+        icon: '🔀',
+      });
+
+      const server = await startService(`server-a.${ext}`, {
+        startAfter: 'ready',
+        cwd:
+          ext === 'ts' || isProduction
+            ? 'fixtures/server'
+            : 'ci/fixtures/server',
+      });
+
+      const res = await legacyFetch('localhost', 4000);
+
+      assert.strictEqual(res?.statusCode, 200, 'Service is on');
+      assert.deepStrictEqual(
+        JSON.parse(res?.body),
+        { name: 'Poku' },
+        'Poku service is online'
+      );
+
+      await server.end();
+    });
+
+    await test(async () => {
+      describe('Start Script (No Ports)', {
+        background: false,
+        icon: '🔀',
+      });
+
+      const server = await startScript(`start:${ext}`, {
+        startAfter: 'ready',
+        cwd:
+          ext === 'ts' || isProduction
+            ? 'fixtures/server'
+            : 'ci/fixtures/server',
+        runner: runtime === 'node' ? 'npm' : runtime,
+      });
+
+      const res = await legacyFetch('localhost', 4001);
+
+      assert.strictEqual(res?.statusCode, 200, 'Script is on');
+      assert.deepStrictEqual(
+        JSON.parse(res?.body),
+        { name: 'Poku' },
+        'Poku script is online'
+      );
+
+      await server.end();
+    });
+  }
 })();
