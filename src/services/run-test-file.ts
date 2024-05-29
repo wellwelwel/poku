24e5f5a6e2d138d7a8f5236af3e65223ff721460
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-05-29 05:48:51 -0300

    chore: use `jsdelivr` to Deno polyfill

--- src/services/run-test-file.ts
+++ src/services/run-test-file.ts
@@ -25,7 +25,7 @@ export const runTestFile = (
       configs?.deno?.cjs === true ||
       (Array.isArray(configs?.deno?.cjs) &&
         configs.deno.cjs.some((ext) => filePath.includes(ext)))
-        ? './node_modules/poku/lib/polyfills/deno.mjs'
+        ? 'https://cdn.jsdelivr.net/npm/poku@experimental/lib/polyfills/deno.mjs'
         : filePath,
     ];
     /* c8 ignore stop */
