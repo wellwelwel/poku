a96a53acdebff7e31df572e279b54605e1612678
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-06-09 17:54:05 -0300

    feat: support for Node.js `test` familiar API usage

--- src/modules/test.ts
+++ src/modules/test.ts
@@ -1,18 +1,39 @@
 /* c8 ignore next */
 import { each } from '../configs/each.js';
+import { describe } from './describe.js';
 
+export async function test(
+  message: string,
+  cb: () => Promise<unknown>
+): Promise<void>;
+export async function test(message: string, cb: () => unknown): Promise<void>;
 export async function test(cb: () => Promise<unknown>): Promise<void>;
 export function test(cb: () => unknown): void;
 export async function test(
-  cb: () => unknown | Promise<unknown>
+  ...args: [
+    string | (() => unknown | Promise<unknown>),
+    (() => unknown | Promise<unknown>)?,
+  ]
 ): Promise<void> {
+  let message: string | undefined;
+  let cb: () => unknown | Promise<unknown>;
+
   if (typeof each.before.cb === 'function' && each.before.test) {
     const beforeResult = each.before.cb();
+
     /* c8 ignore next */
     if (beforeResult instanceof Promise) await beforeResult;
   }
 
+  if (typeof args[0] === 'string') {
+    message = args[0];
+    cb = args[1] as () => unknown | Promise<unknown>;
+  } else cb = args[0] as () => unknown | Promise<unknown>;
+
+  if (message) describe(message, { icon: '›' });
+
   const resultCb = cb();
+
   /* c8 ignore next */
   if (resultCb instanceof Promise) await resultCb;
 
--- test/integration/test/test.test.ts
+++ test/integration/test/test.test.ts
@@ -1,11 +1,30 @@
-import { test } from '../../../src/index.js';
+import { describe, test } from '../../../src/index.js';
 
-(async () => {
+describe('Testing "test" method', {
+  icon: '🔬',
+});
+
+test(async () => {
+  test(() => {});
   test(() => true);
   test(() => false);
   test(() => undefined);
   test(() => new Promise((resolve) => resolve(undefined)));
   test(async () => await new Promise((resolve) => resolve(undefined)));
+
   await test(() => new Promise((resolve) => resolve(undefined)));
   await test(async () => await new Promise((resolve) => resolve(undefined)));
-})();
+});
+
+test(async () => {
+  test('', () => {});
+  test('', () => true);
+  test('', () => false);
+  test('', () => undefined);
+  test('', () => new Promise((resolve) => resolve(undefined)));
+  test('', async () => await new Promise((resolve) => resolve(undefined)));
+
+  await test('', () => new Promise((resolve) => resolve(undefined)));
+  await test('', async () =>
+    await new Promise((resolve) => resolve(undefined)));
+});
