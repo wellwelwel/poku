bf7bc281dbf179d8dccc652e84aa2b31c70e3c9c
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-06-21 18:52:07 -0300

    refactor: remove CLI async scope

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 /* c8 ignore start */
-
 import process from 'node:process';
 import { escapeRegExp } from '../modules/list-files.js';
 import {
@@ -21,150 +20,150 @@ import { onSigint, poku } from '../modules/poku.js';
 import { kill } from '../modules/processes.js';
 import type { Configs } from '../@types/poku.js';
 
-(async () => {
-  const dirs = (() => {
-    const includeArg = getArg('include');
-    if (includeArg !== undefined) return includeArg.split(',');
+const dirs = (() => {
+  const includeArg = getArg('include');
+  if (includeArg !== undefined) return includeArg.split(',');
 
-    const lastParam = getLastParam();
-    if (lastParam !== undefined) return lastParam.split(',');
+  const lastParam = getLastParam();
+  if (lastParam !== undefined) return lastParam.split(',');
 
-    return ['.'];
-  })();
+  return ['.'];
+})();
 
-  const platform = getArg('platform');
-  const filter = getArg('filter');
-  const exclude = getArg('exclude');
-  const killPort = getArg('kill-port');
-  const killRange = getArg('kill-range');
-  const killPID = getArg('kill-pid');
-  const denoAllow = argToArray('deno-allow');
-  const denoDeny = argToArray('deno-deny');
-  const denoCJS =
-    getArg('deno-cjs')
-      ?.split(',')
-      .map((a) => a.trim())
-      .filter((a) => a) || hasArg('deno-cjs');
+const platform = getArg('platform');
+const filter = getArg('filter');
+const exclude = getArg('exclude');
+const killPort = getArg('kill-port');
+const killRange = getArg('kill-range');
+const killPID = getArg('kill-pid');
+const denoAllow = argToArray('deno-allow');
+const denoDeny = argToArray('deno-deny');
+const denoCJS =
+  getArg('deno-cjs')
+    ?.split(',')
+    .map((a) => a.trim())
+    .filter((a) => a) || hasArg('deno-cjs');
 
-  const parallel = hasArg('parallel');
-  const quiet = hasArg('quiet');
-  const debug = hasArg('debug');
-  const failFast = hasArg('fail-fast');
-  const watchMode = hasArg('watch');
+const parallel = hasArg('parallel');
+const quiet = hasArg('quiet');
+const debug = hasArg('debug');
+const failFast = hasArg('fail-fast');
+const watchMode = hasArg('watch');
 
-  const concurrency = parallel
-    ? Number(getArg('concurrency')) || undefined
-    : undefined;
+const concurrency = parallel
+  ? Number(getArg('concurrency')) || undefined
+  : undefined;
 
-  if (killPort) {
-    const ports = killPort.split(',').map(Number);
+const tasks: Promise<unknown>[] = [];
 
-    await kill.port(ports);
+if (killPort) {
+  const ports = killPort.split(',').map(Number);
+  tasks.push(kill.port(ports));
+}
+
+if (killRange) {
+  const ranges = killRange.split(',');
+
+  for (const range of ranges) {
+    const ports = range.split('-').map(Number);
+    const startsAt = ports[0];
+    const endsAt = ports[1];
+    tasks.push(kill.range(startsAt, endsAt));
   }
+}
 
-  if (killRange) {
-    const ranges = killRange.split(',');
+if (killPID) {
+  const PIDs = killPID.split(',').map(Number);
+  tasks.push(kill.pid(PIDs));
+}
 
-    for (const range of ranges) {
-      const ports = range.split('-').map(Number);
+const options: Configs = {
+  platform: platformIsValid(platform) ? platform : undefined,
+  filter: filter ? new RegExp(escapeRegExp(filter)) : undefined,
+  exclude: exclude ? new RegExp(escapeRegExp(exclude)) : undefined,
+  parallel,
+  quiet,
+  debug,
+  failFast,
+  concurrency,
+  noExit: watchMode,
+  deno: {
+    allow: denoAllow,
+    deny: denoDeny,
+    cjs: denoCJS,
+  },
+};
 
-      const startsAt = ports[0];
-      const endsAt = ports[1];
+if (debug) {
+  hr();
+  write(`${format.bg(104, 'Debug Enabled')}\n`);
+  write(`${format.italic(format.info('…'))} ${format.bold('Paths')}`);
+  console.table(dirs);
+  write('\n');
+  write(`${format.italic(format.info('…'))} ${format.bold('Options')}`);
+  console.dir(options, { depth: null, colors: true });
+}
 
-      await kill.range(startsAt, endsAt);
-    }
-  }
+Promise.all(tasks).then(() => {
+  poku(dirs, options).then(() => {
+    if (watchMode) {
+      const executing = new Set<string>();
+      const interval = Number(getArg('watch-interval')) || 1500;
 
-  if (killPID) {
-    const PIDs = killPID.split(',').map(Number);
+      const resultsClear = () => {
+        fileResults.success.clear();
+        fileResults.fail.clear();
+      };
 
-    await kill.pid(PIDs);
-  }
+      process.removeListener('SIGINT', onSigint);
+      resultsClear();
 
-  const options: Configs = {
-    platform: platformIsValid(platform) ? platform : undefined,
-    filter: filter ? new RegExp(escapeRegExp(filter)) : undefined,
-    exclude: exclude ? new RegExp(escapeRegExp(exclude)) : undefined,
-    parallel,
-    quiet,
-    debug,
-    failFast,
-    concurrency,
-    noExit: watchMode,
-    deno: {
-      allow: denoAllow,
-      deny: denoDeny,
-      cjs: denoCJS,
-    },
-  };
+      mapTests('.', dirs, options.filter, options.exclude).then(
+        (mappedTests) => {
+          Array.from(mappedTests.keys()).forEach((mappedTest) => {
+            watch(mappedTest, (file, event) => {
+              if (event === 'change') {
+                const filePath = normalizePath(file);
+                if (executing.has(filePath)) return;
 
-  if (debug) {
-    hr();
-    write(`${format.bg(104, 'Debug Enabled')}\n`);
-    write(`${format.italic(format.info('…'))} ${format.bold('Paths')}`);
-    console.table(dirs);
-    write('\n');
-    write(`${format.italic(format.info('…'))} ${format.bold('Options')}`);
-    console.dir(options, { depth: null, colors: true });
-  }
+                executing.add(filePath);
+                resultsClear();
 
-  await poku(dirs, options);
+                const tests = mappedTests.get(filePath);
+                if (!tests) return;
 
-  if (watchMode) {
-    const executing = new Set<string>();
-    const interval = Number(getArg('watch-interval')) || 1500;
+                poku(Array.from(tests), options).then(() => {
+                  setTimeout(() => {
+                    executing.delete(filePath);
+                  }, interval);
+                });
+              }
+            });
+          });
+        }
+      );
 
-    const resultsClear = () => {
-      fileResults.success.clear();
-      fileResults.fail.clear();
-    };
-
-    process.removeListener('SIGINT', onSigint);
-    resultsClear();
-
-    mapTests('.', dirs, options.filter, options.exclude).then((mappedTests) => {
-      Array.from(mappedTests.keys()).forEach((mappedTest) => {
-        watch(mappedTest, (file, event) => {
+      dirs.forEach((dir) => {
+        watch(dir, (file, event) => {
           if (event === 'change') {
-            const filePath = normalizePath(file);
-            if (executing.has(filePath)) return;
+            if (executing.has(file)) return;
 
-            executing.add(filePath);
+            executing.add(file);
             resultsClear();
 
-            const tests = mappedTests.get(filePath);
-            if (!tests) return;
-
-            poku(Array.from(tests), options).then(() => {
+            poku(file, options).then(() => {
               setTimeout(() => {
-                executing.delete(filePath);
+                executing.delete(file);
               }, interval);
             });
           }
         });
       });
-    });
 
-    dirs.forEach((dir) => {
-      watch(dir, (file, event) => {
-        if (event === 'change') {
-          if (executing.has(file)) return;
-
-          executing.add(file);
-          resultsClear();
-
-          poku(file, options).then(() => {
-            setTimeout(() => {
-              executing.delete(file);
-            }, interval);
-          });
-        }
-      });
-    });
-
-    hr();
-    write(`${format.bold('Watching:')} ${format.underline(dirs.join(', '))}`);
-  }
-})();
+      hr();
+      write(`${format.bold('Watching:')} ${format.underline(dirs.join(', '))}`);
+    }
+  });
+});
 
 /* c8 ignore stop */
