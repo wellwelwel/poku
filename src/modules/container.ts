368dacd2c069211705777a690f1040202c7210a6
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-06-24 02:28:25 -0300

    docs: improve documentation

--- src/modules/container.ts
+++ src/modules/container.ts
@@ -5,7 +5,16 @@ import {
 } from '../@types/container.js';
 import { DockerCompose, DockerContainer } from '../services/container.js';
 
+/**
+ *
+ * A minimal API to assist tests that require containers or tests that run inside containers using a **Dockerfile**.
+ */
 const dockerfile = (configs: DockerfileConfigs) => new DockerContainer(configs);
+
+/**
+ *
+ * A minimal API to assist tests that require containers or tests that run inside containers using a **docker-compose.yml**.
+ */
 const compose = (configs: DockerComposeConfigs) => new DockerCompose(configs);
 
 /* c8 ignore next */ // c8 bug
