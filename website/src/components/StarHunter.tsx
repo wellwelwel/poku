ee1800f8f177937cc2f3ddc12ad15c43eda1c26f
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2024-07-21 15:29:04 -0300

    feat: support for short and alternative flags (#568)
    * feat: support for short and alternative flags
    
    * docs: add docs
    
    * docs: add JSR faq
    
    * chore: use short flags

--- website/src/components/StarHunter.tsx
+++
@@ -1,8 +0,0 @@
-export const StarHunter: React.FC = () => {
-  return (
-    <blockquote className='star-hunter'>
-      <p>Enjoying Poku?</p>
-      <p>Consider giving him a star, he is a good Pet Runner 🩷</p>
-    </blockquote>
-  );
-};
