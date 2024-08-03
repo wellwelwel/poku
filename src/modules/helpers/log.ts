d7da98d3166b0ab0591fd92230f177cb99722ed5
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-08-03 17:30:06 -0300

    feat: allow multiple args and types to `log` helper

--- src/modules/helpers/log.ts
+++ src/modules/helpers/log.ts
@@ -1,4 +1,14 @@
+import { parseResultType } from '../../parsers/assert.js';
 import { Write } from '../../services/write.js';
 
 /** By default **Poku** only shows outputs generated from itself. This helper allows you to use an alternative to `console.log` with **Poku**. */
-export const log = (message: string) => Write.log(`\x1b[0m${message}\x1b[0m`);
+export const log = (...args: unknown[]) => {
+  const parsedMessages = args
+    .map((arg) => parseResultType(arg))
+    .join(' ')
+    .split('\n')
+    .map((line) => `\x1b[0m${line}\x1b[0m`)
+    .join('\n');
+
+  Write.log(parsedMessages);
+};
