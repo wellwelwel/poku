3a6db52d569dee6fb834252567f0684376742154
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-08-03 15:47:08 -0300

    feat: add `--version` flag

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -21,6 +21,7 @@ export { getPIDs } from './helpers/get-pids.js';
 export { exit } from './helpers/exit.js';
 export { log } from './helpers/log.js';
 export { listFiles } from './helpers/list-files.js';
+export { VERSION as version } from '../configs/poku.js';
 
 export type { Code } from '../@types/code.js';
 export type { Configs } from '../@types/poku.js';
