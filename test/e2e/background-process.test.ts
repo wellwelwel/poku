5c1a4dfd7c5d75acbfe099f13bbd2f3998a18306
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-08-14 01:43:03 -0300

    ci: restaure background services tests

--- test/e2e/background-process.test.ts
+++ test/e2e/background-process.test.ts
@@ -10,14 +10,9 @@ import { legacyFetch } from '../__utils__/legacy-fetch.test.js';
 import { ext } from '../__utils__/capture-cli.test.js';
 import { getRuntime } from '../../src/parsers/get-runtime.js';
 import { waitForPort } from '../../src/modules/helpers/wait-for.js';
-import { skip } from '../../src/modules/helpers/skip.js';
 
 test(async () => {
   const runtime = getRuntime();
-
-  if (runtime === 'bun') {
-    skip('TODO: Check what has changed after Bun v1.1.23');
-  }
 
   await describe('Start Service (Single Port)', async () => {
     await it(async () => {
