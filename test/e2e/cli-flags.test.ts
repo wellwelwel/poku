284a3cc371342b2b59e288d52b8dbfa05e21595d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-08-18 16:51:12 -0300

    ci: fix build tests

--- test/e2e/cli-flags.test.ts
+++ test/e2e/cli-flags.test.ts
@@ -1,7 +1,12 @@
 import { describe } from '../../src/modules/helpers/describe.js';
 import { it } from '../../src/modules/helpers/it/core.js';
 import { assert } from '../../src/modules/essentials/assert.js';
-import { inspectPoku } from '../__utils__/capture-cli.test.js';
+import { inspectPoku, isBuild } from '../__utils__/capture-cli.test.js';
+import { skip } from '../../src/modules/helpers/skip.js';
+
+if (isBuild) {
+  skip();
+}
 
 describe('CLI Flags', async () => {
   await it('Short flags', async () => {
@@ -21,8 +26,6 @@ describe('CLI Flags', async () => {
     const output = await inspectPoku('--debug --fail-fast --deno-cjs=js,cjs', {
       cwd: 'test/__fixtures__/e2e/no-tests',
     });
-
-    console.log(output.stdout);
 
     assert.strictEqual(output.exitCode, 0, 'Exit Code needs to be 0');
     assert(/debug:(.+)true/.test(output.stdout), 'CLI needs to able "debug"');
