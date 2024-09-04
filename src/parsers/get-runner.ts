2fc575def6ecb3fb69f714604767c028b6fbe4b9
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-09-04 03:07:33 -0300

    debug: compare performance

--- src/parsers/get-runner.ts
+++ src/parsers/get-runner.ts
@@ -1,4 +1,4 @@
-import type { Configs, Runtime } from '../@types/poku.js';
+import type { Configs } from '../@types/poku.js';
 import type { Runner } from '../@types/runner.js';
 import { platform } from 'node:process';
 import { extname } from 'node:path';
@@ -7,36 +7,36 @@ import { getRuntime } from './get-runtime.js';
 export const isWindows = platform === 'win32';
 
 export const runner = (filename: string, configs?: Configs): string[] => {
-  const command: Record<Runtime, () => string[]> = {
-    node: () =>
-      ['.ts', '.mts', '.cts'].includes(extname(filename))
-        ? [isWindows ? 'npx.cmd' : 'npx', 'tsx']
-        : ['node'],
-    bun: () => ['bun'],
-    deno: () => {
-      const denoAllow = configs?.deno?.allow
-        ? configs.deno.allow
-            .map((allow) => (allow ? `--allow-${allow}` : ''))
-            .filter((allow) => allow)
-        : [
-            '--allow-read',
-            '--allow-env',
-            '--allow-run',
-            '--allow-net',
-            '--allow-hrtime',
-          ];
+  const runtime = getRuntime(configs);
 
-      const denoDeny = configs?.deno?.deny
-        ? configs.deno.deny
-            .map((deny) => (deny ? `--deny-${deny}` : ''))
-            .filter((deny) => deny)
-        : [];
+  if (runtime === 'bun') return ['bun'];
 
-      return ['deno', 'run', ...denoAllow, ...denoDeny];
-    },
-  };
+  if (runtime === 'deno') {
+    const denoAllow = configs?.deno?.allow
+      ? configs.deno.allow
+          .map((allow) => (allow ? `--allow-${allow}` : ''))
+          .filter((allow) => allow)
+      : [
+          '--allow-read',
+          '--allow-env',
+          '--allow-run',
+          '--allow-net',
+          '--allow-hrtime',
+        ];
 
-  return command[getRuntime(configs)]() ?? command['node' as Runtime]();
+    const denoDeny = configs?.deno?.deny
+      ? configs.deno.deny
+          .map((deny) => (deny ? `--deny-${deny}` : ''))
+          .filter((deny) => deny)
+      : [];
+
+    return ['deno', 'run', ...denoAllow, ...denoDeny];
+  }
+
+  // Node.js
+  return ['.ts', '.mts', '.cts'].includes(extname(filename))
+    ? [isWindows ? 'npx.cmd' : 'npx', 'tsx']
+    : ['node'];
 };
 
 export const scriptRunner = (runner: Runner): string[] => {
