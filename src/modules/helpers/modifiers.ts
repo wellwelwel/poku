273b158588a8a6ae5c7d604aec60a3c07fef68df
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-09-10 08:55:22 -0300

    fix: fix typings of `todo` modifier

--- src/modules/helpers/modifiers.ts
+++ src/modules/helpers/modifiers.ts
@@ -2,10 +2,29 @@ import { Write } from '../../services/write.js';
 import { indentation } from '../../configs/indentation.js';
 import { format } from '../../services/format.js';
 
-export const todo = (message: string, _cb?: () => unknown) =>
+export function todo(message: string): void;
+export async function todo(
+  message: string,
+  _cb?: () => Promise<unknown>
+): Promise<void>;
+export function todo(message: string, _cb?: () => unknown): void;
+export async function todo(
+  message: string | (() => unknown) | (() => Promise<unknown>),
+  _cb?: (() => unknown) | (() => Promise<unknown>)
+): Promise<void> {
   Write.log(
     `${indentation.hasDescribe ? '  ' : ''}${format(`● ${message}`).cyan().bold()}`
   );
+
+  /* c8 ignore start */ // Type guard
+  if (typeof _cb === 'function') {
+    const isAsync = _cb.constructor.name === 'AsyncFunction';
+
+    if (isAsync) return await Promise.resolve();
+    return;
+  }
+  /* c8 ignore stop */
+}
 
 export async function skip(
   message: string,
