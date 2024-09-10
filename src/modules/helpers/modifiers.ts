90dc4d94a3543c72ddc95739a90320abc623059a
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-09-10 08:25:31 -0300

    fix: fix typings of `skip` modifier

--- src/modules/helpers/modifiers.ts
+++ src/modules/helpers/modifiers.ts
@@ -7,16 +7,37 @@ export const todo = (message: string, _cb?: () => unknown) =>
     `${indentation.hasDescribe ? '  ' : ''}${format(`● ${message}`).cyan().bold()}`
   );
 
+export async function skip(
+  message: string,
+  _cb: () => Promise<unknown>
+): Promise<void>;
 export function skip(message: string, _cb: () => unknown): void;
+export async function skip(_cb: () => Promise<unknown>): Promise<void>;
 export function skip(_cb: () => unknown): void;
-export function skip(
-  messageOrCb: string | (() => unknown),
-  _cb?: () => unknown
-) {
+export async function skip(
+  messageOrCb: string | (() => unknown) | (() => Promise<unknown>),
+  _cb?: (() => unknown) | (() => Promise<unknown>)
+): Promise<void> {
   const message =
     (typeof messageOrCb === 'string' && messageOrCb) || 'Skipping';
 
   Write.log(
     `${indentation.hasDescribe ? '  ' : ''}${format(`◯ ${message}`).info().bold()}`
   );
+
+  // Type guard
+  if (typeof messageOrCb === 'function') {
+    const isAsync = messageOrCb.constructor.name === 'AsyncFunction';
+
+    if (isAsync) return await Promise.resolve();
+    return;
+  }
+
+  // Type guard
+  if (typeof _cb === 'function') {
+    const isAsync = _cb.constructor.name === 'AsyncFunction';
+
+    if (isAsync) return await Promise.resolve();
+    return;
+  }
 }
