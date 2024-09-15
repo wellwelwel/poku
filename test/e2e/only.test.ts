a0f67e70622f6a3baa3cb503ee9eb2bf73bdad29
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-09-15 18:49:02 -0300

    ci: add tests

--- test/e2e/only.test.ts
+++ test/e2e/only.test.ts
@@ -1,17 +1,33 @@
 import { describe } from '../../src/modules/helpers/describe.js';
 import { it } from '../../src/modules/helpers/it/core.js';
 import { assert } from '../../src/modules/essentials/assert.js';
-import { inspectPoku } from '../__utils__/capture-cli.test.js';
+import { ext, inspectCLI, inspectPoku } from '../__utils__/capture-cli.test.js';
 import { nodeVersion } from '../../src/parsers/get-runtime.js';
 import { skip } from '../../src/modules/helpers/skip.js';
+import { runner } from '../../src/parsers/get-runner.js';
 
 if (nodeVersion && nodeVersion < 12) skip();
 
 describe('Only', async () => {
+  const cmd = runner(`_.${ext}`).join(' ');
+
   await it('Normal usage', async () => {
     const results = await inspectPoku('--only --debug', {
       cwd: 'test/__fixtures__/e2e/only',
     });
+
+    if (results.exitCode !== 0) {
+      console.log(results.stdout);
+      console.log(results.stderr);
+    }
+
+    assert.strictEqual(results.exitCode, 0, 'Passed');
+  });
+
+  await it('No Poku Runner', async () => {
+    const results = await inspectCLI(
+      `${cmd} ./test/__fixtures__/e2e/only/basic-logs.test.${ext} --only`
+    );
 
     if (results.exitCode !== 0) {
       console.log(results.stdout);
@@ -27,6 +43,21 @@ describe('Only', async () => {
     const results = await inspectPoku('--debug', {
       cwd: 'test/__fixtures__/e2e/only',
     });
+
+    if (results.exitCode !== 1) {
+      console.log(results.stdout);
+      console.log(results.stderr);
+    }
+
+    assert.strictEqual(results.exitCode, 1, 'Failed');
+  });
+
+  await it('No Poku Runner should fail without `--only`', async () => {
+    if (nodeVersion && nodeVersion < 16) return;
+
+    const results = await inspectCLI(
+      `${cmd} ./test/__fixtures__/e2e/only/basic-logs.test.${ext}`
+    );
 
     if (results.exitCode !== 1) {
       console.log(results.stdout);
