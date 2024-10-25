c8af91f0afc55f69ade95bc76ddaca9b082fd4b7
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-10-25 18:34:43 -0300

    chore: improve comment

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+SHORT_SHA=$(git rev-parse --short HEAD)
+
+echo '### 🚀 Benchmark Results\n'
+echo '```'
+
 hyperfine -i --warmup 3 --export-json results.json \
-  --command-name 'Jest' 'node --experimental-vm-modules ./node_modules/jest/bin/jest.js ./test/jest' \
-  --command-name 'Mocha' './node_modules/mocha/bin/mocha.js --parallel ./test/mocha' \
-  --command-name 'Vitest' './node_modules/vitest/vitest.mjs run ./test/vitest' \
-  --command-name 'Poku' '../lib/bin/index.js --parallel ./test/poku'
+  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel ./test/poku' \
+  --command-name 'Mocha (10.7.3)' './node_modules/mocha/bin/mocha.js --parallel ./test/mocha' \
+  --command-name 'Jest (29.7.0)' 'node --experimental-vm-modules ./node_modules/jest/bin/jest.js ./test/jest' \
+  --command-name 'Vitest (2.1.3)' './node_modules/vitest/vitest.mjs run ./test/vitest' |
+  awk '/Summary/ {flag=1} flag'
 
-echo '\n--------------------------------------------\n'
+echo '```'
+echo '\n---\n'
+echo '### 🐢 Comparative with Node.js\n'
+echo '```'
 
-# Not included in result.json
+# Not included in results.json
 hyperfine -i --warmup 3 \
   --command-name 'Node.js' 'node --test "./test/node/**.spec.js"' \
-  --command-name 'Poku' '../lib/bin/index.js --parallel --node ./test/poku'
+  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --node ./test/poku' |
+  awk '/Summary/ {flag=1} flag'
 
-echo '\n--------------------------------------------\n'
+echo '```'
+echo '\n---\n'
+echo '### 🍞 Comparative with Bun.sh\n'
+echo '```'
 
-# Not included in result.json
+# Not included in results.json
 hyperfine -i --warmup 3 \
   --command-name 'Bun' 'bun test "test/bun/"' \
-  --command-name 'Poku' '../lib/bin/index.js --parallel --bun ./test/poku'
+  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --bun ./test/poku' |
+  awk '/Summary/ {flag=1} flag'
 
-jq -e -r '.results | sort_by(.mean) | .[0].command == "Poku"' results.json >/dev/null
+echo '```'
+
+jq -e -r ".results | sort_by(.mean) | .[0].command == \"🐷 Poku ($SHORT_SHA)\"" results.json >/dev/null
