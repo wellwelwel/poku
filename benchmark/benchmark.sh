222301f9792df3f741c8cfc658ed101ef4b1005f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-12-09 19:42:44 -0300

    ci: rollback concurrency limit

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -21,7 +21,7 @@ echo '```'
 # Not included in results.json
 hyperfine -i --warmup 3 \
   --command-name 'Node.js' 'node --test "./test/node/**.spec.js"' \
-  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --concurrency=0 --node ./test/poku' |
+  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --node ./test/poku' |
   awk '/Summary/ {flag=1} flag'
 
 echo '```\n'
@@ -31,7 +31,7 @@ echo '```'
 # Not included in results.json
 hyperfine -i --warmup 3 \
   --command-name 'Bun' 'bun test "test/bun/"' \
-  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --concurrency=0 --bun ./test/poku' |
+  --command-name "🐷 Poku ($SHORT_SHA)" '../lib/bin/index.js --parallel --bun ./test/poku' |
   awk '/Summary/ {flag=1} flag'
 
 echo '```\n'
