8e073234393f67e38ecf4f59bfa5ef4ce9554b3c
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-12-21 07:08:58 -0300

    ci: add ordered tests

--- test/e2e/only.test.ts
+++ test/e2e/only.test.ts
@@ -12,12 +12,54 @@ describe('Only', async () => {
       cwd: 'test/__fixtures__/e2e/only/--only',
     });
 
+    const actual = results.stdout.split('\n');
+    const offset =
+      actual.findIndex((line) => line.includes('Running Tests')) + 1;
+
     if (results.exitCode !== 0) {
       console.log(results.stdout);
       console.log(results.stderr);
     }
 
     assert.strictEqual(results.exitCode, 0, 'Passed');
+
+    assert.match(actual[offset + 1], /1/);
+    assert.match(actual[offset + 2], /3/);
+    assert.match(actual[offset + 3], /3/);
+    assert.match(actual[offset + 4], /1/);
+
+    assert.match(actual[offset + 5], /4/);
+    assert.match(actual[offset + 6], /4/);
+    assert.match(actual[offset + 7], /5/);
+    assert.match(actual[offset + 8], /5/);
+
+    assert.match(actual[offset + 9], /8/);
+    assert.match(actual[offset + 10], /9/);
+    assert.match(actual[offset + 11], /9/);
+    assert.match(actual[offset + 12], /10/);
+    assert.match(actual[offset + 13], /10/);
+    assert.match(actual[offset + 14], /11/);
+    assert.match(actual[offset + 15], /11/);
+    assert.match(actual[offset + 16], /8/);
+
+    assert.match(actual[offset + 17], /15/);
+    assert.match(actual[offset + 18], /17/);
+    assert.match(actual[offset + 19], /17/);
+    assert.match(actual[offset + 20], /15/);
+
+    assert.match(actual[offset + 21], /19/);
+    assert.match(actual[offset + 22], /20/);
+    assert.match(actual[offset + 23], /20/);
+    assert.match(actual[offset + 24], /21/);
+    assert.match(actual[offset + 25], /21/);
+    assert.match(actual[offset + 26], /22/);
+    assert.match(actual[offset + 27], /22/);
+    assert.match(actual[offset + 28], /19/);
+
+    assert.match(actual[offset + 29], /23/);
+    assert.match(actual[offset + 30], /25/);
+    assert.match(actual[offset + 31], /25/);
+    assert.match(actual[offset + 32], /23/);
   });
 
   await it('No Poku Runner', async () => {
@@ -90,7 +132,7 @@ describe('Only', async () => {
       `${cmd} ./test/__fixtures__/e2e/only/hooks.test.${ext} --only`
     );
 
-    if (results.exitCode !== 1) {
+    if (results.exitCode !== 0) {
       console.log(results.stdout);
       console.log(results.stderr);
     }
