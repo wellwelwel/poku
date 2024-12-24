25af464c2b87810dd2530d70e1549dab58a432f4
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-12-24 14:11:13 -0300

    chore: adjust runtime for WSL

--- src/parsers/get-runtime.ts
+++ src/parsers/get-runtime.ts
@@ -1,31 +1,24 @@
-import { env, platform } from 'node:process';
+import { env } from 'node:process';
 import { basename } from 'node:path';
-import { execSync } from 'node:child_process';
 import type { Runtime } from '../@types/poku.js';
+
+declare const Deno: unknown;
+declare const Bun: unknown;
 
 export const getRuntime = (): Runtime => {
   const { _, POKU_RUNTIME } = env;
 
   if (POKU_RUNTIME) return POKU_RUNTIME as Runtime;
+  if (typeof Deno !== 'undefined') return 'deno';
+  if (typeof Bun !== 'undefined') return 'bun';
 
-  if (platform !== 'win32') {
-    if (typeof _ === 'string') {
-      const bin = basename(_);
+  if (typeof _ === 'string') {
+    const bin = basename(_);
 
-      if (bin.indexOf('bun') !== -1) return 'bun';
-      if (bin.indexOf('deno') !== -1) return 'deno';
-    }
-
+    if (bin.indexOf('bun') !== -1) return 'bun';
+    if (bin.indexOf('deno') !== -1) return 'deno';
     return 'node';
   }
-
-  const cmd = execSync(
-    `wmic process where (processid=${process.ppid}) get Commandline`,
-    { stdio: 'pipe' }
-  ).toString();
-
-  if (cmd.indexOf(' bun ') !== -1) return 'bun';
-  if (cmd.indexOf(' deno ') !== -1) return 'deno';
 
   return 'node';
 };
