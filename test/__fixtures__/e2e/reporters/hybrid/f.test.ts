75a72d275503ebe06d6185f2812fd42e40b3a5e2
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-01-23 01:46:01 -0300

    chore: improve coverage

--- test/__fixtures__/e2e/reporters/hybrid/f.test.ts
+++ test/__fixtures__/e2e/reporters/hybrid/f.test.ts
@@ -3,4 +3,6 @@ import { describe } from '../../../../../src/modules/helpers/describe.js';
 describe('Describing a Title');
 
 describe.todo('Describing a Todo');
+// @ts-expect-error
+describe.todo();
 describe.skip('Skipping a describe', () => {});
