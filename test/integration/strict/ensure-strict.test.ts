2ac48f4943b5c204549dda925ca30df5c86b6c44
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-07-27 16:33:06 -0300

    ci: skip strict for Deno v1

--- test/integration/strict/ensure-strict.test.ts
+++ test/integration/strict/ensure-strict.test.ts
@@ -2,9 +2,13 @@ import { GLOBAL } from '../../../src/configs/poku.js';
 import { assert } from '../../../src/modules/essentials/assert.js';
 import { describe } from '../../../src/modules/helpers/describe.js';
 import { skip } from '../../../src/modules/helpers/skip.js';
+import { runtimeVersion } from '../../../src/parsers/runtime-version.js';
 
-if (GLOBAL.runtime === 'bun')
+if (GLOBAL.runtime !== 'bun')
   skip('Default assert acts as strict method for Bun');
+
+if (GLOBAL.runtime === 'deno' && runtimeVersion >= 1)
+  skip('Default assert acts as strict method for Deno v1');
 
 describe('Ensure strict', async () => {
   const { strict } = await import('../../../src/modules/essentials/strict.js');
