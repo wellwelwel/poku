e54b8691bc360273e5b45a152e802fb3614ec139
lojhan <viniciuslojhan@gmail.com>
2025-07-28 20:04:51 -0300

    skipped e2e tests in node runtime

--- test/e2e/shared-resources.test.ts
+++ test/e2e/shared-resources.test.ts
@@ -1,33 +1,33 @@
-// import path, { relative } from 'node:path';
-// import { inspectPoku } from '../__utils__/capture-cli.test.js';
-// import { GLOBAL } from '../../src/configs/poku.js';
-// import { assert } from '../../src/modules/essentials/assert.js';
-// import { describe } from '../../src/modules/helpers/describe.js';
-// import { skip } from '../../src/modules/helpers/skip.js';
+import path, { relative } from 'node:path';
+import { inspectPoku } from '../__utils__/capture-cli.test.js';
+import { GLOBAL } from '../../src/configs/poku.js';
+import { assert } from '../../src/modules/essentials/assert.js';
+import { describe } from '../../src/modules/helpers/describe.js';
+import { skip } from '../../src/modules/helpers/skip.js';
 
-// if (GLOBAL.runtime === 'deno') skip();
+if (GLOBAL.runtime === 'node') skip(); // node + tsx wont work with this test
+// because of the way tsx handles spawning processes, it breaks the IPC chain
 
-// const finalPath = path.join(
-//   GLOBAL.cwd,
-//   'test',
-//   '__fixtures__',
-//   'e2e',
-//   'shared-resources',
-//   'test'
-// );
+const finalPath = path.join(
+  'test',
+  '__fixtures__',
+  'e2e',
+  'shared-resources',
+  'test'
+);
 
-// describe('Shared Resources', async () => {
-//   // console.log(relative(GLOBAL.cwd, finalPath));
-//   // const results = await inspectPoku('--debug --sharedResources', {
-//   //   cwd: relative(GLOBAL.cwd, finalPath),
-//   // });
+describe('Shared Resources', async () => {
+  console.log(relative(GLOBAL.cwd, finalPath));
+  const results = await inspectPoku('--debug --sharedResources', {
+    cwd: finalPath,
+  });
 
-//   // if (results.exitCode !== 0) {
-//   //   console.log(results.stdout);
-//   //   console.log(results.stderr);
-//   // }
+  if (results.exitCode !== 0) {
+    console.log(results.stdout);
+    console.log(results.stderr);
+  }
 
-//   // assert.strictEqual(results.exitCode, 0, 'Exit Code needs to be 0');
-//   // assert(/PASS › 3/.test(results.stdout), 'CLI needs to pass 3');
-//   // assert(/FAIL › 0/.test(results.stdout), 'CLI needs to fail 0');
-// });
+  assert.strictEqual(results.exitCode, 0, 'Exit Code needs to be 0');
+  assert(/PASS › 3/.test(results.stdout), 'CLI needs to pass 3');
+  assert(/FAIL › 0/.test(results.stdout), 'CLI needs to fail 0');
+});
