21d8b527a79897f6de28f73f06c9e547c0e3317b
lojhan <viniciuslojhan@gmail.com>
2025-08-01 14:03:00 -0300

    minor adjustment

--- test/__fixtures__/e2e/shared-resources/test/setter-a.test.ts
+++ test/__fixtures__/e2e/shared-resources/test/setter-a.test.ts
@@ -7,8 +7,6 @@ test('should set message in shared resource from file A', async () => {
   const [resource, detach] =
     await getSharedResource<SharedResourceType>('sharedResource');
 
-  resource.addMessage('Message from File A');
-
   await assert.doesNotReject(() => resource.addMessage('Message from File A'));
 
   assert(
