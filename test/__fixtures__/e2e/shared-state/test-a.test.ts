e9e1d7f3090c6983b1fe423d21b4e25e74b71036
lojhan <viniciuslojhan@gmail.com>
2025-11-22 23:02:03 -0300

    test: better assertions

--- test/__fixtures__/e2e/shared-state/test-a.test.ts
+++ test/__fixtures__/e2e/shared-state/test-a.test.ts
@@ -1,13 +1,17 @@
 import { assert } from '../../../../src/modules/essentials/assert.js';
 import { test } from '../../../../src/modules/helpers/test.js';
+import { waitForExpectedResult } from '../../../../src/modules/index.js';
 import { useCounter } from './counter.js';
 
 test('Test A: Increment Counter', async () => {
   const counter = await useCounter();
 
   const current = await counter.get();
-  assert.strictEqual(current, 0, 'Counter should start at 0');
+  assert.strictEqual(current, 0, 'Should start at 0');
 
   const next = await counter.inc();
-  assert.strictEqual(next, 1, 'Counter should be 1 after increment');
+  assert.strictEqual(next, 1, 'Should be 1 after increment');
+
+  await waitForExpectedResult(counter.get, 2);
+  assert.strictEqual(await counter.get(), 2, 'Should be 2 after waiting');
 });
--- test/__fixtures__/e2e/shared-state/test-b.test.ts
+++ test/__fixtures__/e2e/shared-state/test-b.test.ts
@@ -6,8 +6,9 @@ import { useCounter } from './counter.js';
 test('Test B: Verify Counter State', async () => {
   const counter = await useCounter();
 
-  await waitForExpectedResult(counter.get, 1, { timeout: 1000 });
+  await waitForExpectedResult(counter.get, 1);
+  assert.strictEqual(await counter.get(), 1, 'Should be 1 at start of Test B');
 
   const next = await counter.inc();
-  assert.strictEqual(next, 2, 'Counter should be 2 after second increment');
+  assert.strictEqual(next, 2, 'Should be 2 after second increment');
 });
