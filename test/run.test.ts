2c616630372e709640bb186877003193581f2491
lojhan <viniciuslojhan@gmail.com>
2025-12-01 18:21:14 -0300

    chore: disable useless prop

--- test/run.test.ts
+++ test/run.test.ts
@@ -1,7 +1,6 @@
 import { poku } from '../src/modules/essentials/poku.js';
 
 poku(['test/unit', 'test/integration', 'test/e2e'], {
-  sharedResources: true,
   debug: true,
   deno: {
     allow: ['read', 'write', 'env', 'run', 'net'],
