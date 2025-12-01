6d1df295cceaa384ae2509e4c717a7eaa728760e
lojhan <viniciuslojhan@gmail.com>
2025-12-01 18:17:15 -0300

    test: update to use shared resources in tests

--- test/run.test.ts
+++ test/run.test.ts
@@ -1,6 +1,7 @@
 import { poku } from '../src/modules/essentials/poku.js';
 
 poku(['test/unit', 'test/integration', 'test/e2e'], {
+  sharedResources: true,
   debug: true,
   deno: {
     allow: ['read', 'write', 'env', 'run', 'net'],
