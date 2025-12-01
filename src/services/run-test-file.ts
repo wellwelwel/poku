e64cdf7279a539d9a86c4861ebee8ede78e3e037
lojhan <viniciuslojhan@gmail.com>
2025-12-01 09:12:11 -0300

    removed console.log

--- src/services/run-test-file.ts
+++ src/services/run-test-file.ts
@@ -28,7 +28,6 @@ export const runTestFile = async (path: string): Promise<boolean> => {
 
   const stdOut = (data: Buffer): void => {
     output += String(data);
-    console.log(String(data));
   };
 
   const start = hrtime();
