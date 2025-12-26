7d3936721245668e8b07582420ee1dc8847582a4
lojhan <viniciuslojhan@gmail.com>
2025-12-26 20:25:32 -0300

    test: added more cases for importing with CJS and ESM mixed modules

--- test/unit/imports.test.ts
+++ test/unit/imports.test.ts
@@ -197,4 +197,304 @@ describe('parseImports', () => {
       },
     ]);
   });
+
+  it('should parse multiline named and namespace ESM imports from local files', () => {
+    const code = `
+      import {
+        a,
+        B as b,
+      } from './letters.js';
+      import * as foo from './foo.js';`;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: './letters.js',
+        members: [
+          { name: 'a', alias: 'a', type: 'named' },
+          { name: 'B', alias: 'b', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: './foo.js',
+        members: [{ name: '*', alias: 'foo', type: 'namespace' }],
+        kind: 'esm',
+      },
+    ]);
+  });
+
+  it('should parse mixed ESM and CJS with extreme spacing', () => {
+    const code = `
+      import       {       x       ,       y       as       z       }       from       "weird-spacing"       ;
+      const       {       a       :       b       ,       c       }       =       require       (       "cjs-weird"       )       ;
+      import       default_val       from       "default-weird"       ;
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'weird-spacing',
+        members: [
+          { name: 'x', alias: 'x', type: 'named' },
+          { name: 'y', alias: 'z', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'cjs-weird',
+        members: [
+          { name: 'a', alias: 'b', type: 'named' },
+          { name: 'c', alias: 'c', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+      {
+        module: 'default-weird',
+        members: [{ name: 'default', alias: 'default_val', type: 'default' }],
+        kind: 'esm',
+      },
+    ]);
+  });
+
+  it('should parse extremely multiline imports with mixed quotes', () => {
+    const code = `
+      import {
+        first,
+        second as
+          secondAlias,
+        third
+      } from 'multiline-module';
+
+      const {
+        fourth: fourthAlias,
+        fifth
+      } = require("another-module");
+
+      import
+        default_export
+      from
+        "default-module";
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'multiline-module',
+        members: [
+          { name: 'first', alias: 'first', type: 'named' },
+          { name: 'second', alias: 'secondAlias', type: 'named' },
+          { name: 'third', alias: 'third', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'another-module',
+        members: [
+          { name: 'fourth', alias: 'fourthAlias', type: 'named' },
+          { name: 'fifth', alias: 'fifth', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+      {
+        module: 'default-module',
+        members: [
+          { name: 'default', alias: 'default_export', type: 'default' },
+        ],
+        kind: 'esm',
+      },
+    ]);
+  });
+
+  it('should parse imports with trailing commas and odd formatting', () => {
+    const code = `
+      import {
+        alpha,
+        beta,
+        gamma as greekGamma,
+      } from "greek-letters";
+
+      const {
+        delta: deltaAlias,
+        epsilon,
+      } = require('more-letters');
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'greek-letters',
+        members: [
+          { name: 'alpha', alias: 'alpha', type: 'named' },
+          { name: 'beta', alias: 'beta', type: 'named' },
+          { name: 'gamma', alias: 'greekGamma', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'more-letters',
+        members: [
+          { name: 'delta', alias: 'deltaAlias', type: 'named' },
+          { name: 'epsilon', alias: 'epsilon', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+    ]);
+  });
+
+  it('should parse complex mixed imports with different quote styles', () => {
+    const code = `
+      import * as Utils from "utils";
+      const fs = require('fs');
+      import { readFile } from 'fs/promises';
+      const { join: pathJoin } = require("path");
+      import("dynamic-module");
+      import defaultExport, { namedExport } from './local-file.js';
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'utils',
+        members: [{ name: '*', alias: 'Utils', type: 'namespace' }],
+        kind: 'esm',
+      },
+      {
+        module: 'fs',
+        members: [{ name: 'default', alias: 'fs', type: 'default' }],
+        kind: 'cjs',
+      },
+      {
+        module: 'fs/promises',
+        members: [{ name: 'readFile', alias: 'readFile', type: 'named' }],
+        kind: 'esm',
+      },
+      {
+        module: 'path',
+        members: [{ name: 'join', alias: 'pathJoin', type: 'named' }],
+        kind: 'cjs',
+      },
+      {
+        module: 'dynamic-module',
+        members: [],
+        kind: 'dynamic',
+      },
+      {
+        module: './local-file.js',
+        members: [
+          { name: 'namedExport', alias: 'namedExport', type: 'named' },
+          { name: 'default', alias: 'defaultExport', type: 'default' },
+        ],
+        kind: 'esm',
+      },
+    ]);
+  });
+
+  it('should parse imports with unusual line breaks and indentation', () => {
+    const code = `import{x,
+      y as yAlias,z}from"messy-module";
+const{a:aAlias,
+    b,
+c}=require("cjs-messy");
+import
+*
+as
+namespace
+from
+"namespace-messy";`;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'messy-module',
+        members: [
+          { name: 'x', alias: 'x', type: 'named' },
+          { name: 'y', alias: 'yAlias', type: 'named' },
+          { name: 'z', alias: 'z', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'cjs-messy',
+        members: [
+          { name: 'a', alias: 'aAlias', type: 'named' },
+          { name: 'b', alias: 'b', type: 'named' },
+          { name: 'c', alias: 'c', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+      {
+        module: 'namespace-messy',
+        members: [{ name: '*', alias: 'namespace', type: 'namespace' }],
+        kind: 'esm',
+      },
+    ]);
+  });
+
+  it('should parse mixed ESM default and namespace with CJS destructuring', () => {
+    const code = `
+      import defaultVal, * as everything from "mixed-default-namespace";
+      const { prop1, prop2: renamedProp2, prop3 } = require("cjs-destructured");
+      import sideEffect from "side-effect-module";
+      const entireModule = require("entire-cjs-module");
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'mixed-default-namespace',
+        members: [
+          { name: '*', alias: 'everything', type: 'namespace' },
+          { name: 'default', alias: 'defaultVal', type: 'default' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'cjs-destructured',
+        members: [
+          { name: 'prop1', alias: 'prop1', type: 'named' },
+          { name: 'prop2', alias: 'renamedProp2', type: 'named' },
+          { name: 'prop3', alias: 'prop3', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+      {
+        module: 'side-effect-module',
+        members: [{ name: 'default', alias: 'sideEffect', type: 'default' }],
+        kind: 'esm',
+      },
+      {
+        module: 'entire-cjs-module',
+        members: [{ name: 'default', alias: 'entireModule', type: 'default' }],
+        kind: 'cjs',
+      },
+    ]);
+  });
+
+  it('should parse imports with no spaces around braces and operators', () => {
+    const code = `
+      import{a,b as B,c}from"no-spaces";
+      const{x:X,y,z}=require("cjs-no-spaces");
+      import*as cramped from"cramped-namespace";
+    `;
+    const result = parseImports(code);
+    assert.deepStrictEqual(result, [
+      {
+        module: 'no-spaces',
+        members: [
+          { name: 'a', alias: 'a', type: 'named' },
+          { name: 'b', alias: 'B', type: 'named' },
+          { name: 'c', alias: 'c', type: 'named' },
+        ],
+        kind: 'esm',
+      },
+      {
+        module: 'cjs-no-spaces',
+        members: [
+          { name: 'x', alias: 'X', type: 'named' },
+          { name: 'y', alias: 'y', type: 'named' },
+          { name: 'z', alias: 'z', type: 'named' },
+        ],
+        kind: 'cjs',
+      },
+      {
+        module: 'cramped-namespace',
+        members: [{ name: '*', alias: 'cramped', type: 'namespace' }],
+        kind: 'esm',
+      },
+    ]);
+  });
 });
