514215aac4f2b99c9feb3c9b04d08ea7be784d16
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-03-04 03:41:01 -0300

    ci: remove `--predictable`

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,11 +4,11 @@ SHORT_SHA=$(git rev-parse --short HEAD)
 
 HR="\n---\n"
 
-BIN_POKU="node --predictable ./node_modules/poku/lib/bin/index.js"
-BIN_MOCHA="node --predictable ./node_modules/mocha/bin/mocha.js --parallel"
-BIN_JEST="node --predictable --experimental-vm-modules ./node_modules/jest/bin/jest.js"
-BIN_VITEST="node --predictable ./node_modules/vitest/vitest.mjs run"
-BIN_NODE="node --predictable --test"
+BIN_POKU="node ./node_modules/poku/lib/bin/index.js"
+BIN_MOCHA="node ./node_modules/mocha/bin/mocha.js --parallel"
+BIN_JEST="node --experimental-vm-modules ./node_modules/jest/bin/jest.js"
+BIN_VITEST="node ./node_modules/vitest/vitest.mjs run"
+BIN_NODE="node --test"
 
 rm -rf results
 
