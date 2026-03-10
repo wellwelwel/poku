9bfbf272542c8cfed8fd3f8b5370ad6e50664f81
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2026-03-10 10:28:08 -0300

    fix: ensure `sequential` option in config files (#1055)
    * fix: ensure `sequential` option in config files
    
    * chore: remove debug

--- test/e2e/sequential.test.ts
+++ test/e2e/sequential.test.ts
@@ -1,20 +1,43 @@
 import { inspectPoku } from '../__utils__/capture-cli.test.js';
 import { assert } from '../../src/modules/essentials/assert.js';
 import { describe } from '../../src/modules/helpers/describe.js';
+import { it } from '../../src/modules/helpers/it/core.js';
 import { isWindows } from '../../src/parsers/os.js';
 
 describe('Ensure sequential runs', async () => {
-  const results = await inspectPoku(
-    `--debug --sequential --denoAllow=${isWindows ? '"all"' : 'all'}`,
-    {
-      cwd: 'test/__fixtures__/e2e/sequential',
+  await it('via CLI flag', async () => {
+    const results = await inspectPoku(
+      `--debug --sequential --denoAllow=${isWindows ? '"all"' : 'all'}`,
+      {
+        cwd: 'test/__fixtures__/e2e/sequential',
+      }
+    );
+
+    if (results.exitCode !== 0) {
+      console.log(results.stdout);
+      console.log(results.stderr);
     }
-  );
 
-  if (results.exitCode !== 0) {
-    console.log(results.stdout);
-    console.log(results.stderr);
-  }
+    assert.strictEqual(results.exitCode, 0, 'Exit Code needs to be 0');
+  });
 
-  assert.strictEqual(results.exitCode, 0, 'Exit Code needs to be 0');
+  await it('via config file', async () => {
+    const results = await inspectPoku(
+      `--denoAllow=${isWindows ? '"all"' : 'all'}`,
+      {
+        cwd: 'test/__fixtures__/e2e/sequential-config',
+      }
+    );
+
+    if (results.exitCode !== 0) {
+      console.log(results.stdout);
+      console.log(results.stderr);
+    }
+
+    assert.strictEqual(results.exitCode, 0, 'Exit Code needs to be 0');
+    assert(
+      /sequential.+true/.test(results.stdout),
+      'CLI needs to have "sequential" enabled'
+    );
+  });
 });
