2a5f07dfb94a14283ceb2f3e66ef7f75d9825b8b
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-04-03 04:41:29 -0300

    chore: remove dynamic isolation

--- src/modules/essentials/poku.ts
+++ src/modules/essentials/poku.ts
@@ -85,9 +85,6 @@ export async function poku(
         )
       ).flat(1);
 
-  if (!GLOBAL.configs.isolation)
-    GLOBAL.configs.isolation = testFiles.length > 1 ? 'worker' : 'process';
-
   if (GLOBAL.configs.isolation === 'worker') {
     const { createWorkerPool } = await import('../../services/worker-pool.js');
     const poolSize = GLOBAL.configs.sequential
