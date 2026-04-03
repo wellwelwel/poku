4d80861bd524a985ec971fc79a5c9ce80a411c9b
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-04-03 11:39:27 -0300

    chore: worker debug

--- test/e2e/exit-code.test.ts
+++ test/e2e/exit-code.test.ts
@@ -4,93 +4,91 @@ import { describe } from '../../src/modules/helpers/describe.js';
 import { it } from '../../src/modules/helpers/it/core.js';
 
 describe('Poku Runner Suite', async () => {
-  await Promise.all([
-    it(async () => {
-      const code = await poku(
-        ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
-        {
-          noExit: true,
-          quiet: true,
-        }
-      );
-
-      assert.deepStrictEqual(code, 1, 'Testing all paths as a string array');
-    }),
-
-    it(async () => {
-      const code = await poku('./test/__fixtures__/e2e/fail', {
+  await it(async () => {
+    const code = await poku(
+      ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
+      {
         noExit: true,
         quiet: true,
-      });
+      }
+    );
 
-      assert.deepStrictEqual(code, 1, 'Testing a fail path as string');
-    }),
+    assert.deepStrictEqual(code, 1, 'Testing all paths as a string array');
+  });
 
-    it(async () => {
-      const code = await poku('./test/__fixtures__/e2e/success', {
-        noExit: true,
-        quiet: true,
-      });
+  await it(async () => {
+    const code = await poku('./test/__fixtures__/e2e/fail', {
+      noExit: true,
+      quiet: true,
+    });
 
-      assert.deepStrictEqual(code, 0, 'Testing a success path as string');
-    }),
+    assert.deepStrictEqual(code, 1, 'Testing a fail path as string');
+  });
 
-    it(async () => {
-      const code = await poku(['./test/__fixtures__/e2e/success'], {
-        noExit: true,
-        quiet: true,
-      });
+  await it(async () => {
+    const code = await poku('./test/__fixtures__/e2e/success', {
+      noExit: true,
+      quiet: true,
+    });
 
-      assert.deepStrictEqual(code, 0);
-    }),
+    assert.deepStrictEqual(code, 0, 'Testing a success path as string');
+  });
 
-    it(async () => {
-      const code = await poku(
-        ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
-        {
-          noExit: true,
-          filter: /success/,
-          quiet: true,
-        }
-      );
+  await it(async () => {
+    const code = await poku(['./test/__fixtures__/e2e/success'], {
+      noExit: true,
+      quiet: true,
+    });
 
-      assert.deepStrictEqual(code, 0, 'Filter paths that contains "success"');
-    }),
+    assert.deepStrictEqual(code, 0);
+  });
 
-    it(async () => {
-      const code = await poku(
-        ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
-        {
-          noExit: true,
-          filter: /fail/,
-          quiet: true,
-        }
-      );
-
-      assert.deepStrictEqual(code, 1, 'Filter paths that contains "fail"');
-    }),
-
-    it(async () => {
-      const code = await poku(['test/__fixtures__/e2e/fail'], {
+  await it(async () => {
+    const code = await poku(
+      ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
+      {
         noExit: true,
         filter: /success/,
         quiet: true,
-      });
+      }
+    );
 
-      assert.deepStrictEqual(code, 0, 'No files (success filter)');
-    }),
+    assert.deepStrictEqual(code, 0, 'Filter paths that contains "success"');
+  });
 
-    it(async () => {
-      const code = await poku(
-        ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
-        {
-          noExit: true,
-          filter: /\.(m)?(j|t)?s$/,
-          quiet: true,
-        }
-      );
+  await it(async () => {
+    const code = await poku(
+      ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
+      {
+        noExit: true,
+        filter: /fail/,
+        quiet: true,
+      }
+    );
 
-      assert.deepStrictEqual(code, 1, 'Filter by extension');
-    }),
-  ]);
+    assert.deepStrictEqual(code, 1, 'Filter paths that contains "fail"');
+  });
+
+  await it(async () => {
+    const code = await poku(['test/__fixtures__/e2e/fail'], {
+      noExit: true,
+      filter: /success/,
+      quiet: true,
+    });
+
+    assert.deepStrictEqual(code, 0, 'No files (success filter)');
+  });
+
+  await it(async () => {
+    const code = await poku(
+      ['./test/__fixtures__/e2e/success', 'test/__fixtures__/e2e/fail'],
+      {
+        noExit: true,
+        filter: /\.(m)?(j|t)?s$/,
+        quiet: true,
+      }
+    );
+
+    assert.deepStrictEqual(code, 1, 'Filter by extension');
+  });
 });
