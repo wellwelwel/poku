e3d88d84f0ef3c365019439ed43032a70e222a2d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-04-03 04:19:35 -0300

    chore: worker debug

--- tools/build/version.ts
+++ tools/build/version.ts
@@ -2,13 +2,19 @@ import fs from 'node:fs';
 import { assert } from '../../src/modules/essentials/assert.js';
 
 const packageJsonPath = './package.json';
-const cliFilePath = './lib/configs/poku.js';
 const packageJson = fs.readFileSync(packageJsonPath, 'utf8');
 const packageData = JSON.parse(packageJson);
-const cliFileContent = fs.readFileSync(cliFilePath, 'utf8');
-const newContent = cliFileContent.replace(/''/gm, `'${packageData.version}'`);
 
-fs.writeFileSync(cliFilePath, newContent, 'utf8');
+const targets = ['./lib/configs/poku.js', './ci/src/configs/poku.js'];
+
+for (const filePath of targets) {
+  const content = fs.readFileSync(filePath, 'utf8');
+  fs.writeFileSync(
+    filePath,
+    content.replace(/''/gm, `'${packageData.version}'`),
+    'utf8'
+  );
+}
 
 (async () => {
   const { VERSION } = await import('../../lib/configs/poku.js');
