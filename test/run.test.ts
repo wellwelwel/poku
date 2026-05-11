58957a01eed618682dc5d3327cddb723f6e2581f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-05-11 20:41:28 -0300

    chore: remove debug

--- test/run.test.ts
+++ test/run.test.ts
@@ -2,7 +2,6 @@ import { poku } from '../src/modules/essentials/poku.js';
 
 poku(['test/unit', 'test/integration', 'test/e2e'], {
   debug: true,
-  timeout: 30000,
   deno: {
     allow: ['read', 'write', 'env', 'run', 'net'],
   },
