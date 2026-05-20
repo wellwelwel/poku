7ebf9cc4c357f57398c01ee64758879de584d570
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-05-20 17:21:37 -0300

    refactor: prefer primitive resources

--- src/parsers/callback.ts
+++ src/parsers/callback.ts
@@ -3,11 +3,11 @@ const onlyCall = /(?:^|[^.\w$])(?:it|test|describe)\.only\s*\(/;
 export const checkOnly = (cb: unknown): boolean => {
   if (typeof cb !== 'function') return false;
 
-  return onlyCall.test(cb.toString());
+  return onlyCall.test(String(cb));
 };
 
 export const checkNoOnly = (cb: unknown): boolean => {
   if (typeof cb !== 'function') return false;
 
-  return !onlyCall.test(cb.toString());
+  return !onlyCall.test(String(cb));
 };
