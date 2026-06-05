1d529165a83eda64912e95353953e4c95c1939b0
lojhan <viniciuslojhan@gmail.com>
2026-06-05 10:26:23 -0300

    feat: use shared state for retryContext config

--- src/configs/retry.ts
+++ src/configs/retry.ts
@@ -1,7 +1,8 @@
 import type { RetryContext } from '../@types/retry.js';
+import { getSharedState } from './shared-state.js';
 
-export const retryContext: {
+export const retryContext = getSharedState<{
   stack: RetryContext[] | null;
-} = {
+}>('retryContext', {
   stack: null,
-};
+});
